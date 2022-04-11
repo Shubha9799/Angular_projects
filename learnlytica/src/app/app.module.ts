7d23ca96e316ec383c43b6533cb47eda7d07b646
shubhangi kittur <shubhangikittur@gmail.com>
2022-04-11 08:54:30 +0530

    added angular material

--- learnlytica/src/app/app.module.ts
+++ learnlytica/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 
 import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -43,7 +44,8 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   
