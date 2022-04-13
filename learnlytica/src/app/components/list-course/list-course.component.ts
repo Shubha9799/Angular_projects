a0b40ab54036098c32cbebb6a1cdaa0cb3b9417f
shubhangi kittur <shubhangikittur@gmail.com>
2022-04-13 09:54:55 +0530

    added course using json

--- learnlytica/src/app/components/list-course/list-course.component.ts
+++ learnlytica/src/app/components/list-course/list-course.component.ts
@@ -6,22 +6,36 @@ import { ConfigService } from 'src/app/services/config.service';
   templateUrl: './list-course.component.html',
   styleUrls: ['./list-course.component.css']
 })
-export class ListCourseComponent implements OnInit {
-mycourse:any;
-mycourse2:any;
-  constructor(private myConfigs:ConfigService) { }
 
-  ngOnInit(): void {
-    this.myConfigs.getCourseList().subscribe(
-      (sdata)=>{
-        this.mycourse = sdata;
-      }
-    )
-    this.myConfigs.getCourseList().subscribe(
-      (sdata1)=>{
-        this.mycourse2 = sdata1;
-      }
-    )
+  export class ListCourseComponent implements OnInit {
+    cList: any;
+    dList: any;
+    eList: any;
+    fList: any;
+    gList: any;
+  
+    constructor(private call: ConfigService) { }
+  
+    ngOnInit(): void {
+  
+      this.call.getCourseListCourse().subscribe((value) => {
+        this.cList = value
+      });
+      this.call.getCourseListCourse1().subscribe((value1) => {
+        this.dList = value1
+      });
+      this.call.getCourseListCourse2().subscribe((value2) => {
+        this.eList = value2
+      });
+      this.call.getCourseListCourse3().subscribe((value3) => {
+        this.fList = value3
+      });
+      this.call.getCourseListCourse4().subscribe((value4) => {
+        this.gList = value4
+      });
+  
+  
+    }
+  
   }
 
-}
