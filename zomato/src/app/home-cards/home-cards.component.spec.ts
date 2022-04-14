f2d4fe743e5a4e93a15f353c4af0d839144109d5
shubhangi kittur <shubhangikittur@gmail.com>
2022-04-14 08:18:30 +0530

    added components

---
+++ zomato/src/app/home-cards/home-cards.component.spec.ts
@@ -0,0 +1,27 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { HomeCardsComponent } from './home-cards.component';
+
+describe('HomeCardsComponent', () => {
+  let component: HomeCardsComponent;
+  let fixture: ComponentFixture<HomeCardsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomeCardsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeCardsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
