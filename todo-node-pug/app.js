19b41e25b4c9a013b4ddc0b35420d9db80d428f9
shubhangi kittur <shubhangikittur@gmail.com>
2022-04-19 16:03:54 +0530

    added middleware in pug eng

--- todo-node-pug/app.js
+++ todo-node-pug/app.js
@@ -13,6 +13,13 @@ var cartPayment = require('./routes/cartPayment');
 var app = express();
 var router = express.Router(); //added router
 
+//middleware
+let myLoader=function(req,res,next){
+  console.log(`I am in first middleware ${req.url}`)
+  next();
+};
+app.use(myLoader)
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -23,15 +30,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/payment',cartPayment)
 
+
+
+
+
+
+
+
+//second middleware
+let unmatched=function(req,res,next){
+  console.log(`I am in unmatched middleware ${req.url}`)
+  next();
+};
+app.use(unmatched);
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
