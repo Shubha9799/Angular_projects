db82a938ced04a9624bf8f9bff4ea0f9f32b913d
shubhangi kittur <shubhangikittur@gmail.com>
2022-04-19 10:07:40 +0530

    express-application-generator

--- todo-node-noview/routes/users.js
+++ todo-node-noview/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', function (req, res, next) {
+
+  // console.log(req);
+ 
+   res.send('added user into db');
+ });
+ 
+
 module.exports = router;
