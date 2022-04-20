1c5c4c3bb052bcf447def442ea473527edaecf81
shubhangi kittur <shubhangikittur@gmail.com>
2022-04-20 16:48:48 +0530

    added get, post method s and insert , select query

--- api_learn_dbcon/learn_dbconn/routes/users.js
+++ api_learn_dbcon/learn_dbconn/routes/users.js
@@ -2,16 +2,53 @@ var express = require('express');
 var router = express.Router();
 
 //database connection
-let dbConnection=require('./../database/conn').localConnect();
+let dbConnection = require('./../database/conn').localConnect();
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
- dbConnection.query('SELECT * FROM api_learn.user_profile1',(error,results,fields)=>{
-    if(error) throw error;
-      res.send(results);
+  dbConnection.query('SELECT * FROM api_learn.user_profile', (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+
+  });
+  //res.send('respond with a resource');
+});
+
+//post method
+router.post('/profile', (req, res, next) => {
+  let{
+    Fname,
+    Lname,
+    Company,
+    Designation,
+    Address,
+    join_date
+}=req.body
+
+  let insertquery= `INSERT INTO api_learn.user_profile
+  (Fname,Lname,Company,Designation,Address,join_date)
+   VALUES ('${Fname}','${Lname}','${Company}','${Designation}','${Address}','${join_date}')`;
+
+  dbConnection.query(insertquery,(err,result)=>{
+    if(err){
+      res.send(error);
+      throw error;
+    }
+    else{
+res.send('user details added');
+    }
     
+  });
+})
+
+router.get('/profile', function (req, res, next) {
+
+  dbConnection.query('SELECT * FROM api_learn.user_profile', (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+
   });
   //res.send('respond with a resource');
 });
