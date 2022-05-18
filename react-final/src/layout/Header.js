191d7fa363c023ea2c7f08d33f9f76f485c6dbbb
shubhangi kittur <shubhangikittur@gmail.com>
2022-05-18 15:02:33 +0530

    added navbar using bootstrap

--- react-final/src/layout/Header.js
+++ react-final/src/layout/Header.js
@@ -6,32 +6,33 @@ export default  class Header extends React.Component{
         return (
             <>
 
-            
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <Link to="/"><b>Home &nbsp; &nbsp;</b></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/user"><b>About&nbsp; &nbsp;</b></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/list"><b>Course-list&nbsp; &nbsp;</b></Link>
-                        </li>
-                        <li className="nav-item">
-
-                            <Link to="/hooks"><b>Hooks &nbsp; &nbsp;</b></Link>
-
-                        </li>
-
-                    </ul>
-                </div>
-            </nav>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+     <a class="navbar-brand" href="#">
+      <img src="https://www.datocms-assets.com/45470/1631110818-logo-react-js.png" alt="" width="90" height="60" class="d-inline-block align-text-top"/>
+      
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <Link className="nav-link active" aria-current="page"  to="/">Home</Link>
+        </li>
+        <li class="nav-item">
+        <Link className="nav-link active" aria-current="page"  to="/user">About</Link>
+        </li>
+        <li class="nav-item">
+        <Link className="nav-link active" aria-current="page"  to="/list">Courses</Link>
+        </li>
+        <li class="nav-item">
+        <Link className="nav-link active" aria-current="page"  to="/hooks">Hooks</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
             </>
         )
     }
